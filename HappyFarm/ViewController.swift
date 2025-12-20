d6be4805078ee94ec4d022ca4d382d0b98bf473a
maksimvakula <maksim.vakula@orangesoft.co>
2025-12-20 22:46:50 +0300

    initial

--- HappyFarm/ViewController.swift
+++ HappyFarm/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  HappyFarm
-//
-//  Created by Mikita on 20/12/2025.
-//
-
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
