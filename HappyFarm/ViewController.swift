3cef4bdcdccf486e34e854769ec6bddae2d4a615
maksimvakula <maksim.vakula@orangesoft.co>
2025-12-20 22:44:25 +0300

    start

---
+++ HappyFarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyFarm
+//
+//  Created by Mikita on 20/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
